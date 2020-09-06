18cc0c270bf9e1da5f6097fbb6d097ac10441da7
mintaka33 <mintaka33@outlook.com>
2020-09-06 13:14:25 +0800

    track by frame

--- opencv_tracking.py
+++ opencv_tracking.py
@@ -13,24 +13,25 @@ ap.add_argument("-t", "--tracker", type=str, default="kcf",
     help="OpenCV object tracker type")
 args = vars(ap.parse_args())
 
-(major, minor) = cv2.__version__.split(".")[:2]
-
-if int(major) == 3 and int(minor) < 3:
-    tracker = cv2.Tracker_create(args["tracker"].upper())
-else:
-    OPENCV_OBJECT_TRACKERS = {
-        "csrt": cv2.TrackerCSRT_create,
-        "kcf": cv2.TrackerKCF_create,
-        "boosting": cv2.TrackerBoosting_create,
-        "mil": cv2.TrackerMIL_create,
-        "tld": cv2.TrackerTLD_create,
-        "medianflow": cv2.TrackerMedianFlow_create,
-        "mosse": cv2.TrackerMOSSE_create
-    }
-    tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
-
 initBB = None
-fps = None
+tracker = None
+
+def create():
+    (major, minor) = cv2.__version__.split(".")[:2]
+    if int(major) == 3 and int(minor) < 3:
+        tracker = cv2.Tracker_create(args["tracker"].upper())
+    else:
+        OPENCV_OBJECT_TRACKERS = {
+            "csrt": cv2.TrackerCSRT_create,
+            "kcf": cv2.TrackerKCF_create,
+            "boosting": cv2.TrackerBoosting_create,
+            "mil": cv2.TrackerMIL_create,
+            "tld": cv2.TrackerTLD_create,
+            "medianflow": cv2.TrackerMedianFlow_create,
+            "mosse": cv2.TrackerMOSSE_create
+        }
+        tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+    return tracker
 
 if not args.get("video", False):
     print("[INFO] starting video stream...")
@@ -45,36 +46,34 @@ while True:
     if frame is None:
         break
 
-    frame = imutils.resize(frame, width=500)
+    frame = imutils.resize(frame, width=1280)
     (H, W) = frame.shape[:2]
+
 
     if initBB is not None:
         (success, box) = tracker.update(frame)
 
         if success:
+            print('Track:', box)
             (x, y, w, h) = [int(v) for v in box]
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-        fps.update()
-        fps.stop()
 
         info = [
             ("Tracker", args["tracker"]), 
             ("Success", "Yes" if success else "No"),
-            ("FPS", "{:.2f}".format(fps.fps())),
         ]
 
         for (i, (k, v)) in enumerate(info):
             text = "{}: {}".format(k, v)
-            cv2.putText(frame, text, (10, H - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+            #cv2.putText(frame, text, (10, H - ((i * 20) + 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
     cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(0) & 0xFF
 
     if key == ord("s"):
         initBB = cv2.selectROI("Frame", frame, fromCenter=False, showCrosshair=True)
+        tracker = create()
         tracker.init(frame, initBB)
-        fps = FPS().start()
     elif key == ord("q"):
         break
 
