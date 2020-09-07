2acbed5360294ffeb3a03f2864368de1268ab25e
mintaka33 <mintaka33@outlook.com>
2020-09-07 19:43:27 +0800

    enable cmake

--- math.cpp
+++ math.cpp
@@ -1,5 +1,3 @@
-// math.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 // https://numpy.org/doc/stable/reference/generated/numpy.hanning.html
 
@@ -9,51 +7,46 @@
 
 #define PI 3.14159265
 
-#define W 300
-#define H 200
-
-void hanning(int m, double* d)
-{
-    for (size_t i = 0; i < m; i++)
-    {
+void hanning(const int m, double* d)  {
+    for (size_t i = 0; i < m; i++) {
         d[i] = 0.5 - 0.5 * cos(2*PI*i/(m-1));
     }
 }
 
-void cos2d(int w, int h, double* d)
-{
-    double cos_w[W] = {};
-    double cos_h[H] = {};
-    hanning(W, cos_w);
-    hanning(H, cos_h);
+void cos2d(const int w, const int h, double* d) {
+    double* cos_w = new double[w];
+    double* cos_h = new double[h];
+    hanning(w, cos_w);
+    hanning(h, cos_h);
 
-    for (size_t y = 0; y < H; y++)
-    {
-        for (size_t x = 0; x < W; x++)
-        {
-            d[x + W*y] = cos_h[y] * cos_w[x];
+    for (size_t y = 0; y < h; y++) {
+        for (size_t x = 0; x < w; x++) {
+            d[x + w*y] = cos_h[y] * cos_w[x];
         }
     }
+    delete[] cos_w;
+    delete[] cos_h;
 }
 
-void print(double* cos)
-{
-    for (size_t y = 0; y < H; y++)
-    {
-        for (size_t x = 0; x < W; x++)
-        {
-            printf("%f, ", cos[x + W * y]);
+void print(double* cos, const int w, const int h) {
+    for (size_t y = 0; y < h; y++) {
+        for (size_t x = 0; x < w; x++) {
+            printf("%f, ", cos[x + w * y]);
         }
         printf("\n");
     }
 }
 
-int main()
-{
-    double* cos = new double[H*W];
-    cos2d(W, H, cos);
+int main(int argc, int** argv) {
+    const int w = 300, h = 200;
+    double* cos = new double[h*w];
 
-    printf("done\n");
+    for (size_t i = 0; i < 1000; i++) {
+        cos2d(w, h, cos);
+        printf("-");
+    }
+
     delete [] cos;
+    printf("\ndone\n");
     return 0;
 }
