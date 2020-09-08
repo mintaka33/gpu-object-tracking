029962ece066f29b893ba1c620ad31ac95a71142
lli108 <liang.a.li@intel.com>
2020-09-08 16:31:41 +0800

    dft/fft test

--- math.py
+++ math.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy.fft import fft
+import cmath
 
 def cos2d():
     sz = [20, 10]
@@ -26,6 +28,30 @@ def guassian2d():
     np.savetxt('e:\\dist.csv', dist, delimiter=',')
     np.savetxt('e:\\labels.csv', labels, delimiter=',')
 
-guassian2d()
+def dft_fft():
+    a = [11.0, 22.0, 33.0, 44.0]
+    b = fft(np.array(a))
+    print(b)
+    X = [abs(f) for f in b]
+    print(X)
+
+    N = 4
+    c = -2*np.pi/N
+    W0 = cmath.exp(complex(0, 0*c))
+    W1 = cmath.exp(complex(0, 1*c))
+    W2 = cmath.exp(complex(0, 2*c))
+    W3 = cmath.exp(complex(0, 3*c))
+    W4 = cmath.exp(complex(0, 4*c))
+    W6 = cmath.exp(complex(0, 6*c))
+    W9 = cmath.exp(complex(0, 9*c))
+
+    print(W0, W1, W2, W3, W4, W6, W9, sep='\n')
+    print([abs(n) for n in [W0, W1, W2, W3, W4, W6, W9]])
+    print(cmath.exp(complex(0, -2*np.pi)))
+    W = np.array([[W0, W0, W0, W0], [W0, W1, W2, W3], [W0, W2, W4, W6], [W0, W3, W6, W9]])
+    X = np.array(a)
+    F = np.dot(W, X)
+    print(F)
+    print([abs(f) for f in F])
 
 print('\ndone')
